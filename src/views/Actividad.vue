c06437755d3aa25ca6cdf1692d8095545d5873b1
AndresDiazPinto <andresvis565@gmail.com>
2022-04-06 22:46:30 -0500

    Cargue Final
    Se ajustan imagenes de AD, se crea descargable

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -5,15 +5,15 @@
     
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+        .tarjeta.h-100.d-flex.align-items-center
           figure
-            img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/rellenar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
           h2 Repasando conceptos claves para la selección de personal
         p El objetivo de esta actividad es ayudar a repasar los conocimientos adquiridos en este componente formativo, mediante la organización de palabras que permitan estructurar los conceptos que se deben conocer al momento de acceder a una plataforma de empleo. 
         p.mb-4 Se sugiere que antes de realizar esta actividad, se haga la consulta y el estudio de los materiales formativos.
-        .tarjeta.tarjeta--azul.p-3
+        .tarjeta.bgr_17.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Rellenar los espacios en blanco.
@@ -24,15 +24,15 @@
 
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+        .tarjeta.h-100.d-flex.align-items-center
           figure
-            img(src="@/assets/template/completar.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/arrastar-soltar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
           h2 Relación de conceptos de selección de personal.
         p El objetivo de esta actividad es ayudarle a repasar los conocimientos adquiridos en este componente formativo, mediante la identificación y relación de algunos elementos para realizar una hoja de vida. 
         p.mb-4 Se sugiere que antes de realizar esta actividad, haga la consulta y el estudio de los materiales formativos.
-        .tarjeta.tarjeta--azul.p-3
+        .tarjeta.bgr_17.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar
